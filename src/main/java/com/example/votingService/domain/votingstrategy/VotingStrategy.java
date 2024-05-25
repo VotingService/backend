c36af1a3a9c160d6c66d2bf265d2ce39d94a7464
YuliaShvets <yuliia.shvets04@gmail.com>
2024-05-25 16:13:10 +0300

    Added endpoints for voting

--- src/main/java/com/example/votingService/domain/votingstrategy/VotingStrategy.java
+++ src/main/java/com/example/votingService/domain/votingstrategy/VotingStrategy.java
@@ -1,4 +1,27 @@
 package com.example.votingService.domain.votingstrategy;
 
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
+        property = "votingStrategyType",
+        visible = true
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ApprovalVotingStrategy.class, name = "APPROVAL_VOTING_STRATEGY"),
+        @JsonSubTypes.Type(value = InstantRunoffVotingStrategy.class, name = "INSTANT_RUN_OFF_VOTING_STRATEGY"),
+        @JsonSubTypes.Type(value = PluralityVotingStrategy.class, name = "PLURALITY_VOTING_STRATEGY"),
+})
 public interface VotingStrategy {
+
+    VotingStrategyType votingStrategyType = null;
+
+    enum VotingStrategyType {
+        APPROVAL_VOTING_STRATEGY,
+        INSTANT_RUN_OFF_VOTING_STRATEGY,
+        PLURALITY_VOTING_STRATEGY
+    }
 }
